a2b4b099b2896bf079829451449865bce1a58979
Davide Madrisan <davide.madrisan@gmail.com>
2019-02-07 22:27:39 +0100

    Initial commit
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

---
+++ gcd.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# Plot gcd(m,n) for -50 <= m,n <= 50.
+# A different color is associated to each value with the color range
+# pretty evident along the main diagonal.
+#
+#   -- See "Abstract Algebra with Applications - Audrey Terras
+#
+# Copyright (C) 2019 Davide Madrisan <davide.madrisan.gmail.com>
+
+from math import gcd
+import matplotlib.pylab as plt
+import numpy as np
+
+def main():
+    radius = 50
+    r = range(-radius, radius)
+    data = [[gcd(m, n) for m in r] for n in r]
+    matrix = np.matrix(data)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.set_aspect('equal')
+    plt.imshow(matrix, interpolation='nearest', cmap=plt.cm.ocean)
+    plt.colorbar()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
